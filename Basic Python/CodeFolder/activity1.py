0c722d6bc3b4c83e34da374f41ae12549f67abf7
chollsakn <koonnew8688.cookie@gmail.com>
2023-01-19 22:41:05 +0700

    update

---
+++ Basic Python/CodeFolder/activity1.py
@@ -0,0 +1,36 @@
+unit = int(input("Unit: "))
+price = int(input("Price/Unit: "))
+member = input("Are you member?[y/n]: ")
+isMember = ["y", "n"]
+
+discount = 0
+total = unit * price
+amount = 0
+
+if member == isMember[0]:
+    if total <= 500:
+        discount = (10/100) * total
+        amount = total - discount
+    elif total > 500 and total< 1000: 
+        discount = (15/100) * total
+        amount = total - discount
+    elif total > 1000:
+        discount = (20/100) * total
+        amount = total - discount
+
+elif member == isMember[1]:
+    if total <= 500 :
+        discount = (5/100) * total
+        amount = total - discount
+    elif total > 500 and total < 1000:
+        discount = (10/100) * total
+        amount = total - discount
+    elif total > 1000:        
+        discount = (15/100) * total
+        amount = total - discount
+
+print("Total %.2f" %(total))
+print("Discount %.2f" %(discount))
+print("Amount %.2f" %(amount))
+
+
