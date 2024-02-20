f002c28c8f245342a99001ce2bd2c59bb0a91aff
Chollsak <koonnew8688.cookie@gmail.com>
2024-02-21 06:38:46 +0700

    Update

--- The Complete 2024 Web Development Bootcamp/React.JS/my-app/src/index.js
+++ The Complete 2024 Web Development Bootcamp/React.JS/my-app/src/index.js
@@ -15,3 +15,4 @@ import App from "./components/App";
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
