da1138a6c783a54d3a8c9b915d49c90d91771b5c
Chollsak <koonnew8688.cookie@gmail.com>
2024-02-26 04:59:38 +0700

    Update

---
+++ C# Tutorial - Full Course for Beginners/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 42};
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+            friends[2] = "Kevin";
+            friends[3] = "Oscar";
+            friends[4] = "Dwight";
+            Console.WriteLine(luckyNumbers[0]);
+            Console.WriteLine(friends[2]);
+        }
+    }
+}
