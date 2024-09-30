00bfc6f5d8c41e71cc78544b2522ada3d46bdfcb
Choll Sak <koonnew8688.cookie@gmail.com>
2024-09-30 18:51:15 +0700

    save

---
+++ Node.js and Express.js - Full Course/node-tutorial/1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+    console.log('helloworld')
+}, 2000)
+
+console.log('i will run first')
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpacted error
--- Node.js and Express.js - Full Course/node-tutorial/app.js
+++ Node.js and Express.js - Full Course/node-tutorial/app.js
@@ -1,22 +1,40 @@
-// npm -global command, comes with node 
-// npm --version 
+const {readFile, writeFile} = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf-8')
+        const second = await readFile('./content/second.txt', 'utf-8')
+        await writeFile('./content/result-mind-grenade.txt', `This is awesome: ${first} ${second}`)
+        console.log(first, second),  {flag: 'a'}
+    } catch (error) {
+        console.log(error)
+    }
+    
+    
+}
 
-// global dependency - use it in any project 
-// npm install -g <packageName>
-// sudo install -g <packageName> (mac)
+start()
 
-// package.json - manifest file (stores important info about project /package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err,data) => {
+//             if(err){
+//                 reject(err);
+//             }
+//             else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
 
-
-const _ = require('lodash')
-
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flatMapDeep(items)
-
-console.log(newItems)
+// getText('./content/first.txt')
+//     .then(result => {
+//         console.log(result)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
