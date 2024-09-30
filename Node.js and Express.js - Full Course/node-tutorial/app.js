cae206ea78e9016c2c20ac196f2c2496862f42c5
Choll Sak <koonnew8688.cookie@gmail.com>
2024-09-30 16:33:19 +0700

    first

--- Node.js and Express.js - Full Course/node-tutorial/app.js
+++ Node.js and Express.js - Full Course/node-tutorial/app.js
@@ -11,4 +11,12 @@
 // package.json - manifest file (stores important info about project /package)
 // manual approach (create package.json in the root, create properties etc)
 // npm init (step by step, press enter to skip)
-// npm init -y
+// npm init -y (everything default)
+
+
+const _ = require('lodash')
+
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flatMapDeep(items)
+
+console.log(newItems)
